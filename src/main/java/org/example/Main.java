ed4098b28537af36769604ec45acac12d991dceb
Kseniya <xeniya.11ks@gmail.com>
2023-05-02 20:47:48 +0300

    Gitignore added

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "4444 № 44444444";
+        post.patronymic = "Иванович";
+        post.phone = 525252;
+        post.surname = "Иванов";
+        post.subscription = true;
+
+        post.birthday = new FormDate();
+        post.birthday.day = "13";
+        post.birthday.year = "6";
+        post.birthday.month = "1999";
+
+    }
+}
---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Post {
+    FormDate birthday = new FormDate();
+    public String name;
+    public String passport;
+    public String patronymic;
+    public int phone;
+    public String surname;
+    public boolean subscription;
+}
